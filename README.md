553c58d994ca09d2e5eeadd3040058376fa4eed7
Jesus Aguirre <infoaguirrejesus@duck.com>
2023-10-25 02:25:21 -0500

    Base de repository

---
+++ README.md
@@ -0,0 +1,37 @@
+## Micronaut 4.0.6 Documentation
+
+- [User Guide](https://docs.micronaut.io/4.0.6/guide/)
+- [API Reference](https://docs.micronaut.io/4.0.6/api/)
+- [Configuration Reference](https://docs.micronaut.io/4.0.6/guide/configurationreference.html)
+- [Micronaut Guides](https://guides.micronaut.io/)
+---
+- [Micronaut Maven Plugin documentation](https://micronaut-projects.github.io/micronaut-maven-plugin/latest/)
+## Feature http-client documentation
+
+- [Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#nettyHttpClient)
+
+
+## Feature maven-enforcer-plugin documentation
+
+- [https://maven.apache.org/enforcer/maven-enforcer-plugin/](https://maven.apache.org/enforcer/maven-enforcer-plugin/)
+
+
+## Feature object-storage-oracle-cloud documentation
+
+- [Micronaut Object Storage - Oracle Cloud documentation](https://micronaut-projects.github.io/micronaut-object-storage/latest/guide/)
+
+- [https://www.oracle.com/cloud/storage/object-storage/](https://www.oracle.com/cloud/storage/object-storage/)
+
+
+## Feature oracle-cloud-sdk documentation
+
+- [Micronaut Oracle Cloud SDK documentation](https://micronaut-projects.github.io/micronaut-oracle-cloud/latest/guide/)
+
+- [https://docs.cloud.oracle.com/en-us/iaas/Content/API/SDKDocs/javasdk.htm](https://docs.cloud.oracle.com/en-us/iaas/Content/API/SDKDocs/javasdk.htm)
+
+
+## Feature serialization-jackson documentation
+
+- [Micronaut Serialization Jackson Core documentation](https://micronaut-projects.github.io/micronaut-serialization/latest/guide/)
+
+
---
+++ lib/src/main/java/com/example/ProfilePicturesController.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2023 Oracle and/or its affiliates
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MutableHttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.multipart.CompletedFileUpload;
+import io.micronaut.http.server.types.files.StreamedFile;
+import io.micronaut.http.server.util.HttpHostResolver;
+import io.micronaut.http.uri.UriBuilder;
+import io.micronaut.objectstorage.ObjectStorageEntry;
+import io.micronaut.objectstorage.ObjectStorageOperations;
+import io.micronaut.objectstorage.request.UploadRequest;
+import io.micronaut.objectstorage.response.UploadResponse;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
+
+import java.net.URI;
+import java.util.Optional;
+
+import static io.micronaut.http.HttpHeaders.ETAG;
+import static io.micronaut.http.MediaType.IMAGE_JPEG_TYPE;
+
+@Controller(ProfilePicturesController.PREFIX)
+@ExecuteOn(TaskExecutors.IO)
+class ProfilePicturesController implements ProfilePicturesApi {
+
+    static final String PREFIX = "/pictures";
+
+    private final ObjectStorageOperations<?, ?, ?> objectStorage;
+    private final HttpHostResolver httpHostResolver;
+
+    ProfilePicturesController(ObjectStorageOperations<?, ?, ?> objectStorage,
+                              HttpHostResolver httpHostResolver) {
+        this.objectStorage = objectStorage;
+        this.httpHostResolver = httpHostResolver;
+    }
+
+    @Override
+    public HttpResponse<?> upload(CompletedFileUpload fileUpload,
+                                  String userId,
+                                  HttpRequest<?> request) {
+        String key = buildKey(userId);
+        UploadRequest objectStorageUpload = UploadRequest.fromCompletedFileUpload(fileUpload, key);
+        UploadResponse<?> response = objectStorage.upload(objectStorageUpload);
+
+        return HttpResponse
+                .created(location(request, userId))
+                .header(ETAG, response.getETag());
+    }
+
+    private static String buildKey(String userId) {
+        return userId + ".jpg";
+    }
+
+    private URI location(HttpRequest<?> request, String userId) {
+        return UriBuilder.of(httpHostResolver.resolve(request))
+                .path(PREFIX)
+                .path(userId)
+                .build();
+    }
+
+    @Override
+    public Optional<HttpResponse<StreamedFile>> download(String userId) {
+        String key = buildKey(userId);
+        return objectStorage.retrieve(key)
+                .map(ProfilePicturesController::buildStreamedFile);
+    }
+
+    private static HttpResponse<StreamedFile> buildStreamedFile(ObjectStorageEntry<?> entry) {
+        StreamedFile file = new StreamedFile(entry.getInputStream(), IMAGE_JPEG_TYPE).attach(entry.getKey());
+        MutableHttpResponse<Object> httpResponse = HttpResponse.ok();
+        file.process(httpResponse);
+        return httpResponse.body(file);
+    }
+
+    @Override
+    public void delete(String userId) {
+        String key = buildKey(userId);
+        objectStorage.delete(key);
+    }
+}
